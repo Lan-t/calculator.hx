e370991bafedda4dc10323252ff1cc2b2ab8453d
tia <yknms3533642@yahoo.co.jp>
2019-11-16 01:38:44 +0900

    ブラウザで動くようにした

--- Main.hx
+++ Main.hx
@@ -1,27 +1,29 @@
 import Tokenizer;
 import Parser;
 import Calculator;
+import js.Browser;
+import js.html.InputElement;
 
 
 class Main {
-    public static function main() {
-        var code = "23 + 34 * (10 / 2) ";
-        var code2 = "(23 + 34) * (17 % 6)";
+	public static function main() {
+		Browser.window.onload = onload;
+	};
 
-        var tokenizer = new Tokenizer(code);
-        var tokens = tokenizer.tokenize();
-        var parser = new Parser(tokens);
-        var node = parser.parse();
-        var ans = Calculator.calculate(node);
+	private static function onload() {
+		Browser.document.getElementById('button').onclick = calc;
+	}
 
-        
-        var tokenizer2 = new Tokenizer(code2);
-        var tokens2 = tokenizer2.tokenize();
-        var parser2 = new Parser(tokens2);
-        var node2 = parser2.parse();
-        var ans2 = Calculator.calculate(node2);
-
-        trace(ans);
-        trace(ans2);
-    }
+	private static function calc():Int {
+		var formulaDom = cast(Browser.document.getElementById('formula'), InputElement);
+        var ansDom = Browser.document.getElementById('ans');
+        var code = formulaDom.value;
+		var tokenizer = new Tokenizer(code);
+		var tokens = tokenizer.tokenize();
+		var parser = new Parser(tokens);
+		var node = parser.parse();
+		var ans = Calculator.calculate(node);
+        ansDom.innerText = Std.string(ans);
+		return ans;
+	}
 }
